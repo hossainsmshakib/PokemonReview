29131feeefe948713397eb147e955654a2dd90a4
S. M. Shakib Hossain <smsakib2403@gmail.com>
2023-12-09 23:53:42 +0600

    Added Owner

--- PokemonReview/Models/Owner.cs
+++ PokemonReview/Models/Owner.cs
@@ -1,9 +1,9 @@
 ﻿namespace PokemonReview.Models
 {
-    public class Pokemon
+    public class Owner
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public DateTime BirthDate { get; set; }
-    } 
+        public string Gym { get; set; }
+    }
 }
