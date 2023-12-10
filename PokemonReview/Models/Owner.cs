ad5db704681c196b1b93d701ddd138b06f2b359f
S. M. Shakib Hossain <smsakib2403@gmail.com>
2023-12-10 15:54:06 +0600

    Updated Owner

--- PokemonReview/Models/Owner.cs
+++ PokemonReview/Models/Owner.cs
@@ -6,5 +6,6 @@
         public string Name { get; set; }
         public string Gym { get; set; }
         public Country Country { get; set; } // 1-1 realtion
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
     }
 }
