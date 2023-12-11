1e2bb4ce71822c5047b50969efad91e9c3d2d61a
S. M. Shakib Hossain <smsakib2403@gmail.com>
2023-12-11 14:56:40 +0600

    Added Seed and Updated owner, rev, program

--- PokemonReview/Models/Owner.cs
+++ PokemonReview/Models/Owner.cs
@@ -3,7 +3,8 @@
     public class Owner
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Gym { get; set; }
         public Country Country { get; set; } // 1-1 realtion
         public ICollection<PokemonOwner> PokemonOwners { get; set; }
