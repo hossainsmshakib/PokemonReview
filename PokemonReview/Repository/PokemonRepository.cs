22256cbf46fcefea5544ba50ca5b9cba3c35fad9
S. M. Shakib Hossain <smsakib2403@gmail.com>
2023-12-12 14:46:19 +0600

    Added Pokemon Repo

---
+++ PokemonReview/Repository/PokemonRepository.cs
@@ -0,0 +1,14 @@
+﻿using PokemonReview.Data;
+
+namespace PokemonReview.Repository
+{
+    public class PokemonRepository
+    {
+        private readonly DataContext _context;
+        public PokemonRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
