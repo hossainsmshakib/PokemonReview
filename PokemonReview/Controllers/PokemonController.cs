d056e12e85c489151156ca3becdc414c72594db8
S. M. Shakib Hossain <smsakib2403@gmail.com>
2023-12-13 11:41:38 +0600

    Added PokemonController

---
+++ PokemonReview/Controllers/PokemonController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonReview.Interfaces;
+
+namespace PokemonReview.Controllers
+{
+    [Route("api/[controller])")]
+    [ApiController]
+    public class PokemonController
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+        public PokemonController(IPokemonRepository pokemonRepository)
+        {
+            _pokemonRepository = pokemonRepository;
+        }
+    }
+}
