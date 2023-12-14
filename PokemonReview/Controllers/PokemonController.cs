3082666f7a752f4566404afa8ae3a94b18be3b56
S. M. Shakib Hossain <smsakib2403@gmail.com>
2023-12-14 15:34:54 +0600

    Added HTTP Get

--- PokemonReview/Controllers/PokemonController.cs
+++ PokemonReview/Controllers/PokemonController.cs
@@ -1,16 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PokemonReview.Data;
 using PokemonReview.Interfaces;
+using PokemonReview.Models;
 
 namespace PokemonReview.Controllers
 {
     [Route("api/[controller])")]
     [ApiController]
-    public class PokemonController
+    public class PokemonController: Controller
     {
         private readonly IPokemonRepository _pokemonRepository;
-        public PokemonController(IPokemonRepository pokemonRepository)
+        public PokemonController(IPokemonRepository pokemonRepository, DataContext context)
         {
             _pokemonRepository = pokemonRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+
+        public IActionResult GetPokemon()
+        {
+            var pokemons = _pokemonRepository.GetPokemons();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok();
         }
     }
 }
